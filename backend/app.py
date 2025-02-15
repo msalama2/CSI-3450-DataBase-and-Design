bae1b24e22c2a66eb4b21a5795cb28f2fef0597c
msalama2 <147346674+msalama2@users.noreply.github.com>
2025-02-14 22:19:42 -0500

    Updated app.py

--- backend/app.py
+++ backend/app.py
@@ -5,8 +5,6 @@ template_dir = os.path.abspath("frontend/JobManagement")
 
 app = Flask(__name__, template_folder=template_dir)
 
-print("Looking for templates in:", app.template_folder) 
-
 @app.route('/')
 def home():
     return render_template('index.html')
