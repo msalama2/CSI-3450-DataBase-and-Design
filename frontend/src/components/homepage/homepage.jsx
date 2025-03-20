1bfab9c88a33d2fa5d88bb20aa10e3cc9e0ce90b
Benjamin Braniff <Ben.Braniff37@gmail.com>
2025-03-19 20:04:39 -0400

    Connect Tato's popup with find course button in sidebar

--- frontend/src/components/homepage/homepage.jsx
+++ frontend/src/components/homepage/homepage.jsx
@@ -1,6 +1,8 @@
 import "./homepage.css";
 import FindCoursePopup from "./components/findcourse/popup.jsx";
 import logo from "../../assets/Oakland_Golden_Grizzlies_logo.png";
+import settingsIcon from "../../assets/settings icon.png";
+import profileIcon from "../../assets/profile_icon.png";
 
 const Homepage = () => {
   const studentName = "Ben Braniff";
@@ -11,18 +13,21 @@ const Homepage = () => {
       <div className="homepage">
         <div className="topbar">
           <div className="left">
-            <div className="logo">
+            <div className="icon">
               <img src={logo} alt="can't display image" />
             </div>
             <h1 className="title">Student Registration</h1>
           </div>
           <div className="right">
-            <h1 className="name">Hello {studentName}</h1>
-            <div className="settings"></div>
-            <div className="profile"></div>
+            <h1 className="name">Hello {studentName}!</h1>
+            <div className="icon settings">
+              <img src={settingsIcon} alt="can't display image" />
+            </div>
+            <div className="icon profile">
+              <img src={profileIcon} alt="can't display image" />
+            </div>
           </div>
         </div>
-        <FindCoursePopup />
         {/* 
         body
             left sidebar
@@ -34,6 +39,20 @@ const Homepage = () => {
                 columns (days of week)
                     rows (times of day)
         */}
+
+        <div className="body">
+          <div className="sidebar">
+            <select name="cars" id="cars">
+              <option value="option1">Summer 2025</option>
+              <option value="option2">Fall 2025</option>
+              <option value="option3">Winter 2026</option>
+              <option value="option4">Fall 2026</option>
+            </select>
+            <FindCoursePopup />
+            <div className="term-summary">Term Summary</div>
+          </div>
+          <div className="calendar"></div>
+        </div>
       </div>
     </>
   );
