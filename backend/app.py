431ee50bcaead4c83ae3c514281fc88f57685cf8
msalama2 <147346674+msalama2@users.noreply.github.com>
2025-03-27 13:55:18 -0400

    Added another method to search course_id from db

--- backend/app.py
+++ backend/app.py
@@ -3,19 +3,25 @@ from flask import Flask, render_template, jsonify, request
 from datetime import datetime, timedelta
 import traceback
 import jwt
-from methods.user_login import login_user
+from methods.user_login import *
+from methods.courses import *
 
-template_dir = os.path.abspath("frontend/JobManagement")
+template_dir = os.path.abspath("frontend")
 
 app = Flask(__name__, template_folder=template_dir)
 
 @app.route('/')
 def home():
+   
     return render_template('index.html')
 
 @app.route('/login', methods=['POST'])
 def login():
     return login_user(request)
+
+@app.route('/search_course', methods=['GET'])  # Add the route
+def search_course_by_id():
+    return search_course(request)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/database/queries.py
+++ backend/database/queries.py
@@ -17,3 +17,22 @@ def fetch_user_by_email(email):
   except Exception as e:
     print(f"Error fetching user: {e}")
     return None
+  
+def fetch_course_id(course_id):
+    
+    try:  
+        conn = get_db_connection()
+        cursor = conn.cursor(cursor_factory=RealDictCursor)
+
+        query = "SELECT * FROM courses WHERE course_code = %s;"
+        cursor.execute(query, (course_id,))
+         
+        course = cursor.fetchone()
+        cursor.close()
+        conn.close()
+
+        return course
+
+    except Exception as e:
+        print(f"Error fetching course: {e}")
+        return None
